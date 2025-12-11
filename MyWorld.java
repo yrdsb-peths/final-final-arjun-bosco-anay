b5511cf67f4ec551c44cbf817cccf73049877d2a
Minigopal <archer.arjunraj@gmail.com>
2025-12-11 14:32:44 -0500

    added ninja sprite and movement

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 600, 1);
+        Ninja ninja = new Ninja();
+        addObject(ninja, 300, 500);
     }
 }
--- Ninja.java
+++ Ninja.java
@@ -12,8 +12,36 @@ public class Ninja extends Actor
      * Act - do whatever the Ninja wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    int dx = 0;
+    int dy = 0;
+    int speed = 2;
     public void act()
     {
         // Add your action code here.
+        
+        if (Greenfoot.isKeyDown("w"))
+        {
+            dx = 0;
+            dy = -speed;
+        }else if (Greenfoot.isKeyDown("s"))
+        {
+            dx = 0;
+            dy = speed;
+        }else if (Greenfoot.isKeyDown("a"))
+        {
+            dx = -speed;
+            dy = 0;
+        }else if (Greenfoot.isKeyDown("d"))
+        {
+            dx = speed;
+            dy = 0;
+        }else
+        {
+            dx = 0;
+            dy = 0;
+        }
+        
+        setLocation(getX() + dx, getY() + dy);
     }
 }
