2056093865864c548523e8306b6df9d6425cffa1
Bosco-sigma <fbmeng2009@gmail.com>
2026-01-10 11:20:10 -0500

    added a new Button subclass that tracks to see if the user wants to restart or exit the game

---
+++ Button.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Button here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Button extends Actor
+{
+    /**
+     * Act - do whatever the Button wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private String action; // Signals "start", "restart", or "exit"
+    
+    public Button(String text, String action)
+    {
+        this.action = action;
+        
+        GreenfootImage img = new GreenfootImage(200, 50);
+        img.setColor(Color.GRAY);
+        img.fill();
+        img.setColor(Color.WHITE);
+        img.drawString(text, 50, 30);
+        setImage(img);
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(this))
+        {
+            if(action.equals("start") || action.equals("restart"))
+            {
+                Greenfoot.setWorld(new MyWorld());
+            }
+            else if(action.equals("exit"))
+            {
+                Greenfoot.setWorld(new StartScreen());
+            }
+        }
+    }
+}
--- StartScreen.java
+++ StartScreen.java
@@ -17,11 +17,7 @@ public class StartScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 600, 1); 
-        showStartButton();
+        
     }
     
-    private void showStartButton()
-    {
-        GreenfootImage btn = new GreenfootImage("
-    }
 }
